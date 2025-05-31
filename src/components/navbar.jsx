130f80fbe57a43254d57d0f93e50343fb617d241
ranjesh <rs0036870@gmail.com>
2025-05-31 21:49:28 +0545

    made changes to nav bar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,22 @@
+import { BiNotepad } from "react-icons/bi";
+
 const Navbar = () => {
   return (
-    <nav className="flex justify-between bg-slate-900 text-white py-2.5 ">
-      <div className="logo">
-        <span className="fonr-bold text-xl mx-9">todos</span>
+    <nav className="flex justify-between items-center bg-slate-900 text-white py-2.5 w-full h-15">
+      <div className="logo flex items-center gap-0.1 ml-12">
+        <BiNotepad size={24} className="mx-1" />
+        <span className="font-bold text-xl mx-2">Todo</span>
       </div>
-      <ul className="flex gap-9 mx-9">
+
+      <ul className="flex gap-10 mx-9 pr-11">
         <li className="cursor-pointer hover:font-bold trasition-all duration-50">
-          home
+          Home
         </li>
         <li className="cursor-pointer hover:font-bold trasition-all duration-50">
-          todos
+          About
+        </li>
+        <li className="cursor-pointer hover:font-bold trasition-all duration-50">
+          Finished
         </li>
       </ul>
     </nav>
